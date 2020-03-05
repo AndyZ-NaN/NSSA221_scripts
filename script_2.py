d545543f9e1d356c6d68e71910ee74f007fe186d
Andy Zhu <jxz3986@rit.edu>
2020-03-05 12:55:14 -0500

    Added functionality that validates the records to be added using useradd

--- script_2.py
+++ script_2.py
@@ -29,14 +29,16 @@ PHONE_IND = 4
 DEPT_IND = 5
 GROUP_IND = 6
 
-# Gets user info at its raw state
 users_info = []
+not_added = []
+added = []
+
+# Gets user info at its raw state
 csv = raw_input("Enter the name of the csv : ")
 csv = "Lab02_Users.csv"
 read_csv(csv, users_info)
 
 #After read, check for records that have a length greater than 7 and see if there is something to remove
-print(users_info)
 for user in users_info:
     if user[7] == '':
         #print(user[7])
@@ -45,4 +47,47 @@ for user in users_info:
     if len(user) > 7 and '' in user:
         print("Found empty in user: " + ','.join(user))
         user.remove("")
-        #print(user)
+        #print(user)        
+
+print(users_info)
+
+#Check if EMPID even exists
+
+#Check if first name is alphas non-numeric only
+for user in users_info:
+    #If any of first or last names has a non-alpha in it, chances are that the field is wrong
+    if not (user[LNAME_IND].isalpha() and user[FNAME_IND].isalpha()):
+        print("There is an error with employee ID %s's name record. User not added" % (user[EMPID_IND]))
+        not_added.append(user)
+        continue
+    #Else create username for user
+    else:
+        username = user[FNAME_IND][0] + user[LNAME_IND]
+        #Check if new username is a dupe
+        username_count = sum(x.count(username) for x in added)
+        if(username_count > 0):
+            username = username + str(username_count)
+    #Check for dept and group
+    if user[DEPT_IND].strip() == '' or user[GROUP_IND].strip() == '':
+        print("There is critical information regarding the Employee %s's department and group missing. User not added" % (user[EMPID_IND]))
+        not_added.append(user)
+        continue
+    #If the info is present, save to the list
+    else:
+        dept = user[DEPT_IND]
+        group = user[GROUP_IND]
+    #Check non-critical office and phone info
+    if user[OFFICE_IND].strip() == '' or user[PHONE_IND].strip == '' or user[PHONE_IND] == "unlisted":
+        print("Employee %s's office and phone numbers are missing. User will be added without this info" % (user[EMPID_IND]))
+        office = ''
+        phone = ''
+    else:
+        office = user[OFFICE_IND]
+        phone = user[PHONE_IND]
+    added.append([username, office, phone, dept, group])
+
+for line in not_added:
+    print(line)
+print("ADDED:")
+for line in added:
+    print(line)
