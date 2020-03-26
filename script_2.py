e0c4ba57743cff99f6f6f2e266a12106e77a7ebe
Andy Zhu <jxz3986@rit.edu>
2020-03-26 15:04:08 -0400

    Cleaned up part 1 of script

--- script_2.py
+++ script_2.py
@@ -1,6 +1,16 @@
 # Andy Zhu Scripting Assignment 2
 
 from re import sub
+import os
+
+#Header indexes
+EMPID_IND = 0
+LNAME_IND = 1
+FNAME_IND = 2
+OFFICE_IND = 3
+PHONE_IND = 4
+DEPT_IND = 5
+GROUP_IND = 6
 
 def read_csv(file_item, record_list):
     # Reads lines of data from a csv file and appends them to a list
@@ -21,73 +31,77 @@ def has_numbers(input_str):
 def format_name(name):
     return sub('[^A-Za-z0-9]+', '', name).lower()
 
-#Header indexes
-EMPID_IND = 0
-LNAME_IND = 1
-FNAME_IND = 2
-OFFICE_IND = 3
-PHONE_IND = 4
-DEPT_IND = 5
-GROUP_IND = 6
+def shift_cells(record_list):
+    for user in record_list:
+        if user[7] == '':
+            # print(user[7])
+            user.pop()
+            # print(len(user))
+        if len(user) > 7 and '' in user:
+            user.remove("")
+            
+def filter_entries(record_list, new_users):
+    usernames = []
+    for user in record_list:
+        # New dictionary for user listing
+        listing = {}
+        listing["EmployeeID"] = user[EMPID_IND]
+        # If any of first or last names has a non-alpha in it, chances are that the field is wrong
+        if has_numbers(user[LNAME_IND]) or has_numbers(user[FNAME_IND]):
+            print("CRITICAL ERROR: Employee ID %s's name record missing information. User will NOT be added" % (user[EMPID_IND]))
+            # not_added.append(user)
+            continue
+        # Else create username for user
+        else:
+            username = format_name(user[FNAME_IND])[0] + format_name(user[LNAME_IND])
+            # Ensure no duplicate usernames
+            username_count = sum(x.count(username) for x in usernames)
+            if(username_count > 0):
+                username = username + str(username_count)
+            listing["username"] = username
+            usernames.append(username)
+        # Check for dept and group
+        if user[DEPT_IND].strip() == '' or user[GROUP_IND].strip() == '':
+            print("CRITICAL ERROR: Employee %s's department and group missing. User will NOT be added" % (user[EMPID_IND]))
+            # not_added.append(user)
+            continue
+        # If the info is present, save to the list
+        else:
+            listing["department"] = user[DEPT_IND]
+            listing["group"] = user[GROUP_IND]
+        # Check non-critical office and phone info
+        if user[OFFICE_IND].strip() == '':
+            print("Employee %s's office number is missing. User will be added without this info" % (user[EMPID_IND]))
+            user[OFFICE_IND] = ''
+        listing["office"] = user[OFFICE_IND]
+        if user[PHONE_IND].strip == "" or user[PHONE_IND] == "unlisted":
+            print("Employee %s's phone number is missing. User will be added without this info" % (user[EMPID_IND]))
+            user[PHONE_IND] = ''
+        listing["phone"] = user[PHONE_IND]
+        new_users.append(listing)
+
+#===========================================================#
+#===========================================================#
 
 users_info = []
-not_added = []
-added = []
+users_to_be_added = []
 
 # Gets user info at its raw state
-csv = raw_input("Enter the name of the csv : ")
+csv = input("Enter the name of the csv : ")
 # lol
 csv = "Lab02_Users.csv"
 read_csv(csv, users_info)
 
 # After read, check for records that have a length greater than 7 and see if there is something to remove
-for user in users_info:
-    if user[7] == '':
-        # print(user[7])
-        user.pop()
-        # print(len(user))
-    if len(user) > 7 and '' in user:
-        user.remove("")
-        
-# Check if EMPID even exists
+shift_cells(users_info)
+filter_entries(users_info, users_to_be_added)
 
-# Check if first name is alphas non-numeric only
-for user in users_info:
-    # If any of first or last names has a non-alpha in it, chances are that the field is wrong
-    if has_numbers(user[LNAME_IND]) or has_numbers(user[FNAME_IND]):
-        print("There is an error with employee ID %s's name record. User not added" % (user[EMPID_IND]))
-        not_added.append(user)
-        continue
-    # Else create username for user
-    else:
-        username = format_name(user[FNAME_IND])[0] + format_name(user[LNAME_IND])
-        # Check if new username is a dupe
-        username_count = sum(x.count(username) for x in added)
-        if(username_count > 0):
-            username = username + str(username_count)
-    # Check for dept and group
-    if user[DEPT_IND].strip() == '' or user[GROUP_IND].strip() == '':
-        print("There is critical information regarding the Employee %s's department and group missing. User not added" % (user[EMPID_IND]))
-        not_added.append(user)
-        continue
-    # If the info is present, save to the list
-    else:
-        dept = user[DEPT_IND]
-        group = user[GROUP_IND]
-    # Check non-critical office and phone info
-    if user[OFFICE_IND].strip() == '':
-        print("Employee %s's office number is missing. User will be added without this info" % (user[EMPID_IND]))
-    office = user[OFFICE_IND]
-    if user[PHONE_IND].strip == '' or user[PHONE_IND] == "unlisted":
-        print("Employee %s's phone number is missing. User will be added without this info" % (user[EMPID_IND]))
-        phone = ''
-    else:
-        phone = user[PHONE_IND]
-    added.append([username, office, phone, dept, group])
-    
-print("NOT ADDED:")
-for line in not_added:
-    print(line)
-print("ADDED:")
-for line in added:
-    print(line)
+# List out all users to be added to the system
+for listing in users_to_be_added:
+    print(listing)
+
+'''
+    if user[GROUP_IND]
+    os.system("groupadd -f %s")
+    os.system("useradd -m -d /home/%s/%s -s /bin/bash -g %s
+'''
